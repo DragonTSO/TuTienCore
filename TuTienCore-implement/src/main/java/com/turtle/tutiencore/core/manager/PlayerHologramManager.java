1ca3dde1f1064cea66580e66a19561f62a8c67b6
ROG <soninvox@gmail.com>
2026-05-26 23:42:17 +0700

    Use shared compact format for player hologram

--- TuTienCore-implement/src/main/java/com/turtle/tutiencore/core/manager/PlayerHologramManager.java
+++ TuTienCore-implement/src/main/java/com/turtle/tutiencore/core/manager/PlayerHologramManager.java
@@ -438,12 +438,7 @@ public class PlayerHologramManager implements Listener {
     }
 
     private String formatCompact(double number) {
-        if (number < 1000) {
-            return String.valueOf((long) number);
-        }
-        int exp = (int) (Math.log(number) / Math.log(1000));
-        char suffix = "kMGTPE".charAt(Math.min(exp - 1, 5));
-        return String.format("%.1f%c", number / Math.pow(1000, exp), suffix);
+        return RealmManager.formatNumber((long) number);
     }
 
     private void syncFallbackNameTeams() {
